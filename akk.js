b46ee75a6428e5f1e3b8c150a40a411712ebf78e
Searinox <searinox@gmx.de>
2018-11-06 01:09:50 +0100

    +

--- akk.js
+++ akk.js
@@ -421,6 +421,17 @@
 }(typeof self === 'undefined' ? this : self, function () {
   /* eslint-enable strict, func-names, prefer-arrow-callback */
   'use strict'
-  console.warn('Hallo Werner!')
-  return {}
+  const akk = {
+    cfg: {
+        paths: {
+        'sugar'      : ['https://cdnjs.cloudflare.com/ajax/libs/sugar/2.0.4/sugar.min'],
+        'lodash'     : ['https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min'],
+        'ramda'      : ['https://cdnjs.cloudflare.com/ajax/libs/ramda/0.25.0/ramda.min'],
+        'localforage': ['https://cdnjs.cloudflare.com/ajax/libs/localforage/1.7.3/localforage.min'],
+        'blueimp-md5': ['https://cdnjs.cloudflare.com/ajax/libs/blueimp-md5/2.10.0/js/md5.min'],
+        'akk'        : [`https://kelturio.github.io/Akk/akk.js?_=${Date.now()}`],
+      }
+    }
+  }
+  return akk
 }))
